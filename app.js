b4e812aff0386c978fc794c47e67f534bfb83739
ipyiywy <ipyiywy@gmail.com>
2021-08-13 10:58:10 +0800

    upload project

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const restaurantsList = require('./restaurants.json')
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+res.render('index', { restaurants: restaurantsList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+const restaurant = restaurantsList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+const restaurants = restaurantsList.results.filter(restaurant => { return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase())})
+if(restaurantsi.trim().length = 0){
+res.render('index', {restaurants: restaurants, keyword: req.query.keyword })
+} else {
+res.render('index', { restaurants: 
+}
+})
+
+app.listen(port, () => {
+console.log(`Express is running on http://localhost:${port}`)
+})
+
+
